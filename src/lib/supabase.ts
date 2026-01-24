7dd547de787bd9c8ce955b4b0a6384e7ff1189c0
farhad-jadir <amzadbari@gmail.com>
2026-01-24 07:42:13 +0600

    supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 export async function isAdmin() {
-  const { data: { user } } = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   if (!user) return false;
 
